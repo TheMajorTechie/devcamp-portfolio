bdcb9e3da9c73b63e902998ac9ee6c260574209c
Jordan Hudgens <jordan@devcamp.com>
2017-06-14 16:56:18 +0000

    Added portfolio items via the resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,13 @@ ActiveRecord::Schema.define(version: 20170614163446) do
     t.datetime "updated_at",       null: false
   end
 
+    create_table "portfolios", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "body"
+    t.text     "main_image"
+    t.text     "thumb_image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 end
