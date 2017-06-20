c7e3a84386021b721e34c7c8410fb3ae455bbae3
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 16:40:33 +0000

    Custom actions for blog status updates

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 end
