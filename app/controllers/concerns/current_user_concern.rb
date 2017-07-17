f11ac59afc51e85a289da67340775b305a84e38b
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 17:04:12 +0000

    Implemented authorization in engine in portfolios and blogs

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -7,9 +7,11 @@ module CurrentUserConcern
   end
   
   def guest_user
-    OpenStruct.new(name: 'Guest', 
-                  first_name: 'Guest', 
-                  last_name: 'User', 
-                  email: 'guest@guest123.com')
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@guest.com"
+    guest
   end
 end
