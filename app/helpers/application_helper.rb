1d14f98bfa31676c363eded8807ac294e40b9ed2
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 14:19:51 +0000

    Refactored alerts function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -67,4 +67,32 @@ module ApplicationHelper
     "active" if current_page? path
   end
   
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+      
+      if alert
+        alert_generator alert
+      end
+
+  end
+  
+  def alert_generator msg
+    js add_gritter(msg, title: "THE GREAT INTERWEBZ SAYS:", sticky: false, time: 5000,)
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
